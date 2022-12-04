22ad1491de63abd2227823535ee4aa741c83fc0a
Stainley Lebron <saiyamans@gmail.com>
2022-12-03 22:09:21 -0500

    login design

---
+++ app/src/main/java/ca/appolizer/outreach/register/ui/RegisterActivity.java
@@ -0,0 +1,16 @@
+package ca.appolizer.outreach.register.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.appolizer.outreach.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
